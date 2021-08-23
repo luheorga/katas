24a5e0a099e2f074c6ea2f5c4d9b1051343a994a
Ray <rayiqui@hotmail.com>
2021-08-23 16:32:41 -0500

    Sum_of_Perfect_Squares

---
+++ Sum_of_Prefect_Squares.java
@@ -0,0 +1,61 @@
+
+public class Sum_of_Prefect_Squares {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	   System.out.println(sumaEnteros(19));
+	}
+	
+	
+	public static int sumaEnteros(int numero) {
+		
+		int suma=0;
+		int resultado =0;
+		int contador=0;
+		
+		for (int i = numero; i > 0; i--) {
+			
+			
+			if(Math.sqrt(i) % 1 ==0) {
+				
+				System.out.print(i + ",");
+			    suma += i;
+			    contador++; 
+			    
+			    if (numero == (i*2)) {
+			    	
+			    	resultado = 2; 
+			    	i =0;
+			    }
+			    
+			    else if (numero == (i*2)+1) {
+			    	
+			    	resultado = 3; 
+			    	i =0;
+			    }
+			    
+			    else if (suma > numero && i!=1) {	
+			    	suma -= i;
+			    	contador--;
+			    }
+			    
+			    else if ( suma == numero) {
+			    	resultado = contador;
+			    }
+			    
+			    else if ( i== 1) {
+			    	   	
+			    	resultado = numero - suma + contador;
+			    }
+			    
+			    
+			}
+			
+		}
+		
+		return resultado;
+		
+	}
+
+}
